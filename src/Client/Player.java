2613be183c8f2090594048b63bb79a5e90737896
Marco <marc-antoine.delannoy@univ-tlse3.fr>
2018-12-12 15:04:43 +0100

    Répartition des salles en fonction du nombre de serveur

--- src/Client/Player.java
+++ src/Client/Player.java
@@ -11,7 +11,7 @@ public class Player {
     private static IServerController mainServer;
     private static IGameServer obj;
 
-    private static void moveAvatar(Avatar av, String way, IGameServer gameServer) throws RemoteException {
+    private static int moveAvatar(Avatar av, String way, IGameServer gameServer) throws RemoteException {
         int res = gameServer.move(av, way);
         while(res==-1){
             System.out.println("Il y a pas moyen de passer par là");
@@ -21,9 +21,14 @@ public class Player {
         if(res==-2){
             System.out.println("Case non géré par le serveur");
             obj = mainServer.findGameServer(av.getPosition());
+            if(obj==null){
+                System.out.println("aucun serveur trouvé");
+                return -1;
+            }
         }
         av.setPosition(res);
         System.out.println("Vous êtes arrivé sur la case n°" + av.getPosition());
+        return 0;
     }
 
 
@@ -46,6 +51,10 @@ public class Player {
             //IGameServer obj = (IGameServer) Naming.lookup("//localhost/Dungeon");
             mainServer = (IServerController) Naming.lookup("//localhost/Dungeon");
             obj = mainServer.findGameServer(0);
+            if(obj==null){
+                System.out.println("Aucun serveur trouvé");
+                return;
+            }
             if(obj.connection(avTest.getName(), 0)==1) {
                 avTest.setPosition(0);
                 System.out.println("Connected");
