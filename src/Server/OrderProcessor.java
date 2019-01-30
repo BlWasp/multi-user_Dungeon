a40e4547d8627762cf7e1ba2c0e94ad4b0a92062
Roumy82 <remy.ciubotariu@hotmail.fr>
2019-01-30 13:57:58 +0100

    Ajout OrderProcessor

--- src/Server/OrderProcessor.java
+++ src/Server/OrderProcessor.java
@@ -5,6 +5,9 @@ import Client.Player;
 
 import java.rmi.RemoteException;
 
+/**
+ * Classe d'un gestionnaire d'ordre
+ */
 public class OrderProcessor {
     private final Player p;
     private IGameServer gameserver;
@@ -18,30 +21,61 @@ public class OrderProcessor {
         this.serverController = serverController;
     }
 
+    /**
+     *
+     * @return
+     */
     public IGameServer getGameserver() {
         return gameserver;
     }
 
+    /**
+     *
+     * @param gameserver
+     */
     public void setGameserver(IGameServer gameserver) {
         this.gameserver = gameserver;
     }
 
+    /**
+     *
+     * @return
+     */
     public IChatServer getChatserver() {
         return chatserver;
     }
 
+    /**
+     *
+     * @param chatserver
+     */
     public void setChatserver(IChatServer chatserver) {
         this.chatserver = chatserver;
     }
 
+    /**
+     *
+     * @return
+     */
     public ServerController getServerController() {
         return serverController;
     }
 
+    /**
+     *
+     * @param serverController
+     */
     public void setServerController(ServerController serverController) {
         this.serverController = serverController;
     }
 
+    /**
+     * Gère l'ordre
+     * @param order
+     * @param av
+     * @return
+     * @throws RemoteException
+     */
     public int process (String[] order, Avatar av) throws RemoteException {
         switch (order[0]) {
             case "Move":
@@ -61,6 +95,9 @@ public class OrderProcessor {
                 System.out.println("Unknown order, please enter one of the following orders : Move, Attack, Escape, Exit or start your order with \" to chat");
         }
 
+        /**
+         * Appelle le serveur de chat
+         */
         if(order[0].charAt(0) == '\"') {
             // Vérifier s'il y a d'autres joueurs dans la pièce
             // Si oui -> lancer le serveur de chat
